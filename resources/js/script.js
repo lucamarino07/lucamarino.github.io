83f2eedd1169fe6bb1f38e75b20565dc16326c0a
lucamarino07 <43494927+lucamarino07@users.noreply.github.com>
2019-06-11 11:00:05 +0200

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,13 @@
+let imageProfile = document.querySelector("#profilPicture");
+
+let Type = new Typed('#typed', {
+    strings: ['an Engineer', 'a Data Scientist', " an ERP Consultant"],
+    typeSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+}) 
+
+let TypeMotto = new Typed('#type-motto', {
+    strings: ['think of #insights'],
+    typeSpeed: 100,
+}) 

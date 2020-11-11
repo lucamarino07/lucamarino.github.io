2fe8210c36ff98ac2a9aa6eed81cc0ac382fdfeb
lucamarino07 <43494927+lucamarino07@users.noreply.github.com>
2020-11-11 10:33:48 +0100

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 let imageProfile = document.querySelector("#profilPicture");
 
 let Type = new Typed('#typed', {
-    strings: ['an Engineer', 'a Data Scientist', " an ERP Consultant"],
+    strings: ['a Data Scientist', 'an Engineer', ],
     typeSpeed: 70,
     backDelay: 1000,
     loop: true,
